6c5a6ee04fc50f1304b460b1608be3ed5f313465
jaehyuk2-lee <107346845+jaehyuk2-lee@users.noreply.github.com>
2024-05-13 13:06:09 +0900

    Update 04_2753_윤년.cpp

--- 1. tutorial/3. condition/04_2753_윤년.cpp
+++ 1. tutorial/3. condition/04_2753_윤년.cpp
@@ -10,6 +10,7 @@ int main()
     cin >> year;
 
     if (((year%4==0)&&(year%100!=0))||(year%400==0)) cout << 1 << endl;
+    // 윤년의 조건: 4의배수면서 100의 배수는 아니거나, 400의 배수
     else cout << 0 << endl;
 
     return 0;
