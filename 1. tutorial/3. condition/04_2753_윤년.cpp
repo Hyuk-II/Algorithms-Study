5b20bb880d78008c4abf6befc9bc5dd411b08144
jaehyuk2-lee <107346845+jaehyuk2-lee@users.noreply.github.com>
2024-05-13 13:05:24 +0900

    Create 04_2753_윤년.cpp

---
+++ 1. tutorial/3. condition/04_2753_윤년.cpp
@@ -0,0 +1,16 @@
+// 2024.03.12
+// 윤년 -> 1, 아니면 -> 0 출력
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+    cin >> year;
+
+    if (((year%4==0)&&(year%100!=0))||(year%400==0)) cout << 1 << endl;
+    else cout << 0 << endl;
+
+    return 0;
+}
