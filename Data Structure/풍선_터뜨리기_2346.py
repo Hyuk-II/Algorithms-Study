5a63b7f92c740fe171eb671962b3ae3d78a93bdd
JAEHYUK LEE <107346845+jaehyuk2-lee@users.noreply.github.com>
2025-10-09 22:41:10 +0900

    2025.10.09

---
+++ Data Structure/풍선_터뜨리기_2346.py
@@ -0,0 +1,39 @@
+# 풍선의 값 만큼 이동해 터뜨릴 시 해당 번호 출력, 풍선 안의 값 만큼 다음 풍선으로 이동 -> rotate 사용
+# 맨 왼쪽 값을 오른쪽으로 이동시 (양수로 회전) -> 터뜨려야 하는 값이 맨 마지막 -> deque pop
+# 맨 오른쪽 값을 왼쪽으로 이동시 (음수로 회전) -> 터뜨려야 하는 값이 맨 앞 -> deque popleft
+
+import sys
+from collections import deque
+
+
+class baloon:
+    def __init__(self, num, value):
+        self.num = num
+        self.value = value
+
+    def p(self):
+        print(self.num, self.value)
+
+
+queue = deque()
+n = int(input())
+values = sys.stdin.readline().split()
+
+for i in range(n):
+    queue.append(baloon(i + 1, int(values[i])))
+
+
+b = queue.popleft()
+rotate = b.value
+print(1, end=" ")
+
+while len(queue) > 0:
+    queue.rotate(-rotate)
+
+    if rotate < 0:
+        b = queue.popleft()
+    elif rotate > 0:
+        b = queue.pop()
+
+    print(b.num, end=" ")
+    rotate = b.value
